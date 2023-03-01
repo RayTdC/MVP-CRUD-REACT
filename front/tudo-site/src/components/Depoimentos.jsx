3021844cb6190de8318b192d62b4643aece43770
EsterM99 <112878619+EsterM99@users.noreply.github.com>
2023-03-01 09:52:22 -0300

    Incluindo texto
    sobre a loja

--- front/tudo-site/src/components/Depoimentos.jsx
+++ front/tudo-site/src/components/Depoimentos.jsx
@@ -12,8 +12,7 @@ function Depoimentos() {
             <Col sm="12" md="10">
                 <strong>Depoimento 1</strong>
                 <p>
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus at voluptate voluptatibus illo ad vel ut numquam, quas sunt hic esse, dolore recusandae est vero quod minima eius repellat."
+                    "Super indico a loja. Além do atendimento maravilhoso as roupas são de excelente qualidade,trabalham só com os melhores tecidos.As roupas não desbotam,não dão bolinhas,não encolhem... simplesmente maravilhosas.O atendimento é nota 1000.Qualquer dúvida na hora da compra eles auxiliam e o envio tb é super rápido, chegando sempre antes do prazo. Sou fã da Moda e Magia ♥"
                 </p>
             </Col>
         </Row>
@@ -24,8 +23,18 @@ function Depoimentos() {
             <Col sm="12" md="10">
                 <strong>Depoimento 2</strong>
                 <p>
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.<br />
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas repellendus at voluptate voluptatibus illo ad vel ut numquam, quas sunt hic esse, dolore recusandae est vero quod minima eius repellat."
+                "Perfeito. Desde o início, excelente atendimento, recebi muito rápido, muito cuidado e delicadeza com a embalagem, produto lindo e de boa qualidade. Indico e irei virar cliente. Parabéns!."
+                </p>
+            </Col>
+        </Row>        
+        <Row className='mt-3'>
+            <Col sm="12" md="2" className='text-center'>
+                <img src="https://via.placeholder.com/100/ffd000" alt="Foto do depoimento 2" className='rounded' />
+            </Col>
+            <Col sm="12" md="10">
+                <strong>Depoimento 3</strong>
+                <p>
+                "Só admiração por essa loja, fui a loja física e fui muito bem atendida, time nota 1000. Parabéns meninas, o atendimento faz toda a diferença em uma empresa, voltarei mais vezes."
                 </p>
             </Col>
         </Row>        
--- front/tudo-site/src/pages/Sobre.jsx
+++ front/tudo-site/src/pages/Sobre.jsx
@@ -18,7 +18,15 @@ function Sobre() {
 
     return (
         <Container className='conteudo-margin'>
-            <h1>Sobre</h1>
+            <h1>Sobre Nossa Loja</h1>
+
+
+            <h4>Nascemos em um pequeno cantinho, no interior de uma galeria, com 15 metros quadrados, em agosto de 2011. De lá pra cá, muuuita coisa mudou. Para melhor, é claro. Com o passar dos anos, fomos nos adaptando e aprendendo muito com cada uma das pessoas que por aqui passou. Passamos a compreender que a moda vai muito além de seguir tendências. Entendemos que se vestir é uma forma de comunicação, tem a ver com estilo individual de vida e precisa nos fazer sentir bem.
+Nossa meta é construir e manter, uma relação de confiança e amizade com você, através do nosso atendimento e acolhimento. Acreditamos que a Moda pode (e deve) ser Consciente, Descomplicada e Versátil.
+
+Para te apresentar a nossa proposta, realizamos uma curadoria minuciosa, com peças selecionadas a dedo e que sempre conversam entre si. Te demonstramos o quanto é possível consumir com consciência, ser descomplicada e abusar da versatilidade. Ahh, não podíamos esquecer de te contar o nosso toque final: aqui, em cade detalhe, você vai encontrar muuuuuito amor!
+
+Vem fazer parte do nosso mundo!!! ♥</h4>
             <p>{sobre.text} </p>
             <Depoimentos />
         </Container>
